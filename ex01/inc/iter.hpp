047e60b914a3fb94e3bc7a6d6d569778d7b50334
Maurice <mauricetrautner@gmail.com>
2023-12-06 12:46:29 +0100

    old version of ex01

---
+++ ex01/inc/iter.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtrautne <mtrautne@student.42wolfsburg.d>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 17:46:44 by mtrautne          #+#    #+#             */
+/*   Updated: 2023/12/05 17:46:44 by mtrautne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+template<typename T>
+void	iter(T* arrayAddress, size_t arrayLength, F (*function)(T, size_t)) {
+	(*function)(T)
+
+}
+
+#endif
---
+++ ex01/src/main.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtrautne <mtrautne@student.42wolfsburg.d>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 13:22:14 by mtrautne          #+#    #+#             */
+/*   Updated: 2023/12/05 17:46:01 by mtrautne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include "../inc/iter.hpp"
+
+int	main(void) {
+	std::cout << "I am a main function" << std::endl;
+}
